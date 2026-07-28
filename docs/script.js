b14652e512312ca21d8e79b59d41eeaf6eec1c77
lucasinglocal <lucasinglocal@gmail.com>
2026-07-28 11:36:01 -0300

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -51,8 +51,9 @@
       'ctx.lede':   'A indústria de videogames é uma das mais lucrativas da economia criativa. Todavia, sua produção e reconhecimento se concentram majoritariamente no Norte Global: apenas <b>China (US$ 47 bi)</b> e <b>Estados Unidos (US$ 46,1 bi)</b> respondem por quase metade do faturamento global da indústria de games.',
       'ctx.source': 'Fonte',
       'ctx.lede2': 'As assimetrias do mercado global impulsionam o fenômeno da <b>glocalização</b>, a fusão estratégica entre dinâmicas globais e especificidades locais. No desenvolvimento de jogos, essa tensão se traduz em duas frentes: a <em>generalização</em>, adotando neutralidade estética para mitigar riscos financeiros, e a <em>autenticidade cultural</em> para construir valor e diferenciação competitiva no cenár io internacional.',
-      'ctx.source2': 'Glocal Games',
-       
+      'common.insight0': 'Glocal Games',
+      'icg.insight0': 'jogos produzidos em contextos nacionais específicos que incorporam intencionalmente a cultura local sob padrões técnicos globais.'
+          
       'about.tag':   'Sobre o projeto',
       'about.title': 'Uma interface estratégica de inteligência cultural.',
       'about.lede':  '<b>Glocal Games</b> é uma pesquisa aplicada que descreve os efeitos da glocalização na indústria global de videogames por meio de um estudo transversal que abrange o període de 2011 a fevereiro de 2026. Utilizando como amostra empírica jogos produzidos no Sul Global, a pesquisa desenvolve e aplica o Índice de Glocalização Criativa, instrumento estruturado a partir das dimenções: <em>identidade cultural</em>, e <em>desempenho internacional</em>.',
